ff93a8d5c3968857f07991bba2664a50067cc3e6
ant <a.lhuede@outlook.com>
2018-07-12 20:17:36 +1000

    backend done - calculates results from schedule table

--- tipping-app-backend/app.js
+++ tipping-app-backend/app.js
@@ -29,4 +29,71 @@ app.get('/schedule', async (req, res) =>
   }
 })
 
+app.get('/results', async (req, res, next) => 
+{
+  try {
+    const db = await dbPromise;
+    const schedule = await db.all('SELECT * FROM schedule');
+
+    var result = {};
+    for(var key in schedule)
+    {
+      var item = schedule[key];
+      console.log(item);
+
+      //home team
+      getResult(result, item.HomeTeam, item.HomeResult, item.AwayResult);
+      //away team
+      getResult(result, item.AwayTeam, item.AwayResult, item.HomeResult);
+    }
+
+    var arr = [];
+    for(var key in result)
+    {
+      arr.push({
+        TeamName: key,
+        Wins: result[key].Wins,
+        Losses: result[key].Losses,
+        Difference: result[key].Difference,
+        Points: result[key].Points
+      });
+    }
+    res.send(arr);
+  } catch (err) {
+    next(err);
+  }
+})
+function getResult(result, teamName, teamResult, opponentResult)
+{
+  //won / lost
+  //difference points
+  
+  // var points = 2;
+  // if(won == false)
+  // {
+  //   points = 0;
+  // }
+  //ternary
+
+  //initialize the teams results if it doesnt exist
+  if(result[teamName] === undefined)
+  {
+    result[teamName] = {
+      Wins: 0,
+      Losses: 0,
+      Points: 0,
+      Difference: 0
+    };
+  }
+
+  var won = teamResult > opponentResult ? true : false;
+  var points = won ? 2 : 0;
+  var difference = teamResult - opponentResult;
+
+  result[teamName].Wins += won ? 1 : 0;
+  result[teamName].Losses += won ? 0 : 1;
+  result[teamName].Points += points;
+  result[teamName].Difference += difference;
+}
+
 app.listen(4000, () => console.log('Example app listening on port 4000!'))
