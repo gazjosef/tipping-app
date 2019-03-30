2317d25f549d5ccbdfe38c5e7340d8435962dbc8
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-03-30 15:06:47 +1100

    Updates

---
+++ precache-manifest.6b533aed2f43edd6b87a58969e588896.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f21cee6dd7ff460a9fcf",
+    "url": "/tipping-app/./static/css/main.f76ee42d.chunk.css"
+  },
+  {
+    "revision": "f21cee6dd7ff460a9fcf",
+    "url": "/tipping-app/./static/js/main.a989efab.chunk.js"
+  },
+  {
+    "revision": "b1faf87a9a7480940e92",
+    "url": "/tipping-app/./static/js/runtime~main.15409cd9.js"
+  },
+  {
+    "revision": "081df4299e34bcbb1a39",
+    "url": "/tipping-app/./static/css/2.a29d16ca.chunk.css"
+  },
+  {
+    "revision": "081df4299e34bcbb1a39",
+    "url": "/tipping-app/./static/js/2.197443b4.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/tipping-app/./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/tipping-app/./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/tipping-app/./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/tipping-app/./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/tipping-app/./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "2309f7652e308e0832ed0a261579ec64",
+    "url": "/tipping-app/./index.html"
+  }
+];
