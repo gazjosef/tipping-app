b1429b1d8ba683a454fe050f4e310c6cb45a9a05
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-07-28 00:46:14 +1000

    Updates

--- precache-manifest.7ad56ccc87bf2f82b86e4f2d06d657bb.js
+++ precache-manifest.7ad56ccc87bf2f82b86e4f2d06d657bb.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "274c56001e863a57ec33",
+    "revision": "e2de468fd1af573d8e22",
     "url": "/tipping-app/./static/css/main.80c9cf8c.chunk.css"
   },
   {
-    "revision": "274c56001e863a57ec33",
-    "url": "/tipping-app/./static/js/main.072a6c5d.chunk.js"
+    "revision": "e2de468fd1af573d8e22",
+    "url": "/tipping-app/./static/js/main.c1021218.chunk.js"
   },
   {
     "revision": "b1faf87a9a7480940e92",
@@ -24,12 +24,12 @@ self.__precacheManifest = [
     "url": "/tipping-app/./static/media/glyphicons-halflings-regular.f4769f9b.eot"
   },
   {
-    "revision": "fa2772327f55d8198301fdb8bcfc8158",
-    "url": "/tipping-app/./static/media/glyphicons-halflings-regular.fa277232.woff"
-  },
-  {
     "revision": "448c34a56d699c29117adc64c43affeb",
     "url": "/tipping-app/./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/tipping-app/./static/media/glyphicons-halflings-regular.fa277232.woff"
   },
   {
     "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/tipping-app/./static/media/glyphicons-halflings-regular.89889688.svg"
   },
   {
-    "revision": "5ca02216d3338cf9a01e0e4afe122f2c",
+    "revision": "aa3428ee51ca76f1cf53067997536c1c",
     "url": "/tipping-app/./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tipping-app/./precache-manifest.33ea21c6feb2e19ef3050ec2c73d4335.js"
+  "/tipping-app/./precache-manifest.7ad56ccc87bf2f82b86e4f2d06d657bb.js"
 );
 
 workbox.clientsClaim();
