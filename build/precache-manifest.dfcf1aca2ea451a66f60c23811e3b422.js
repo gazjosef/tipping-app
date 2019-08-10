73910362e0d951392798cf24b4ac8f61b63f87f2
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-08-10 13:42:56 +1000

    Created league table sidebar

---
+++ build/precache-manifest.dfcf1aca2ea451a66f60c23811e3b422.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac4c8b18f7502a287ce",
+    "url": "/tipping-app/static/css/main.80c9cf8c.chunk.css"
+  },
+  {
+    "revision": "fac4c8b18f7502a287ce",
+    "url": "/tipping-app/static/js/main.18b88980.chunk.js"
+  },
+  {
+    "revision": "b8cb3995c2e48e2565ea",
+    "url": "/tipping-app/static/js/runtime~main.71602086.js"
+  },
+  {
+    "revision": "91eb50d9551d0d1b9111",
+    "url": "/tipping-app/static/css/2.999c190b.chunk.css"
+  },
+  {
+    "revision": "91eb50d9551d0d1b9111",
+    "url": "/tipping-app/static/js/2.197443b4.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/tipping-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/tipping-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/tipping-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/tipping-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/tipping-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "014d419c2a93d15d2ecd67a6069ad71b",
+    "url": "/tipping-app/index.html"
+  }
+];
--- src/components/SideTable/SideTable.js
+++ src/components/SideTable/SideTable.js
@@ -1,7 +1,102 @@
 import React, { Component } from "react";
 
 class SideTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fixtures: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://tipping-app-api.herokuapp.com/table", {
+      method: "get",
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => response.json())
+      .then(fixtures => {
+        this.setState({
+          fixtures: fixtures
+        });
+      });
+  }
+
   render() {
+    const leagueTable = this.state.fixtures.map((fixture, index) => {
+      // Arrays
+      let allNames = [];
+      let teamNames = [];
+      let table = [];
+
+      // Collect all team names
+      allNames.push(fixture.home);
+      allNames.push(fixture.away);
+
+      // Remove duplicate names
+      for (let i = 0; i < allNames.length; i++) {
+        if (teamNames.indexOf(allNames[i]) === -1) {
+          teamNames.push(allNames[i]);
+        }
+      }
+      console.log(teamNames);
+
+      // Create League Table
+      teamNames.forEach(name => {
+        let teamScore = {
+          name: name,
+          wins: 0,
+          losses: 0,
+          homePointsScored: 0,
+          homePointsConceded: 0,
+          awayPointsScored: 0,
+          awayPointsConceded: 0,
+          points: 0
+        };
+        table.push(teamScore);
+      });
+
+      fixture.forEach(fixture => {
+        let homeTeam = table.find(team => team.name === fixture.home);
+        let awayTeam = table.find(team => team.name === fixture.away);
+
+        homeTeam.homePointsScored += +fixture.resulthome;
+        homeTeam.homePointsConceded += +fixture.resultaway;
+        awayTeam.awayPointsScored += +fixture.resultaway;
+        awayTeam.awayPointsConceded += +fixture.resulthome;
+
+        if (parseInt(fixture.resulthome) > parseInt(fixture.resultaway)) {
+          homeTeam.wins += 1;
+          homeTeam.points += 2;
+          awayTeam.losses += 1;
+        }
+        if (parseInt(fixture.resultaway) > parseInt(fixture.resulthome)) {
+          awayTeam.wins += 1;
+          awayTeam.points += 2;
+          homeTeam.losses += 1;
+        }
+      });
+
+      // Sort league table by points
+      table.sort((a, b) => {
+        let x = a.points;
+        let y = b.points;
+        if (x < y) {
+          return 1;
+        }
+        if (y < x) {
+          return -1;
+        }
+      });
+      console.log(table);
+
+      //console.log(fixture);
+      return (
+        <div className="tipping-row">
+          <div className="squad away-squad text-right">
+            <h1>Hello World</h1>
+          </div>
+        </div>
+      );
+    });
     return (
       <div className="SideTable">
         {/* Side Table */}
@@ -24,6 +119,7 @@ class SideTable extends Component {
                 </tr>
               </thead>
               <tbody>
+                {leagueTable}
                 <tr>
                   <th scope="row">1</th>
                   <td />
