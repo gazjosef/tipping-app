efa973dbe8e0ca7d276790ca014d88abe664bfc0
Gareth Hind <garethjhind@gmail.com>
2021-02-16 21:53:24 +1100

    Updated Settings

--- src/components/Pages/Settings/Settings.js
+++ src/components/Pages/Settings/Settings.js
@@ -6,6 +6,7 @@ export const Settings = () => {
         <section id="settings">
             {/* <!-- Profile Settings--> */}
             <div className="col-lg-8 pb-5">
+                <h3>Settings</h3>
                 <form className="row">
                     <div className="col-md-6">
                         <div className="form-group">
@@ -46,14 +47,13 @@ export const Settings = () => {
                         </div>
                     </div>
                     <div className="col-12">
-                        <hr className="mt-2 mb-3" />
+                        {/* <hr className="mt-2 mb-3" /> */}
                         <div className="d-flex flex-wrap justify-content-between align-items-center">
                             <div className="custom-control custom-checkbox d-block">
                                 {/* <input className="custom-control-input" type="checkbox" id="subscribe_me" checked="" />
                                 <label className="custom-control-label" htmlFor="subscribe_me">Subscribe me to Newsletter</label> */}
                             </div>
-                            <div className="float-right"></div>
-                            <button className="btn btn-style-1 btn-primary float-right" type="button" data-toast="" data-toast-position="topRight" data-toast-type="success" data-toast-icon="fe-icon-check-circle" data-toast-title="Success!" data-toast-message="Your profile updated successfuly.">Update Profile</button>
+                            <button className="btn btn-style-1 btn-primary btn-settings" type="button" data-toast="" data-toast-position="topRight" data-toast-type="success" data-toast-icon="fe-icon-check-circle" data-toast-title="Success!" data-toast-message="Your profile updated successfuly.">Update Profile</button>
                         </div>
                     </div>
                 </form>
