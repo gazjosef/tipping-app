da54b6ca9fda85ee55306a30249bbad4376e2d1f
Gareth Hind <garethjhind@gmail.com>
2021-03-24 15:45:16 +1100

    Updated tipping app

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { SideTable } from "../../Layout/SideTable/SideTable";
 
-// import Tips from '../../Pages/Tips/Tips';
-import { Tips2 } from '../../Pages/Tips/Tips2';
+import Tips from '../../Pages/Tips/Tips';
+// import { Tips2 } from '../../Pages/Tips/Tips2';
 import { Results } from '../../Pages/Results/Results';
 
 import { Comp } from "../../Pages/Comp/Comp"
@@ -19,11 +19,11 @@ export const Home = () => {
                         <div className="row">
                         <SideTable />
                         <Switch>
-                            {/* <Route exact path="/" component={Tips} /> */}
+                            <Route exact path="/" component={Tips} />
                             {/* <Route exact path="/" component={Tips2} /> */}
                             {/* <Route exact path="/" component={Comp} /> */}
                             {/* // <Route exact path="/" component={Results} /> */}
-                            <Route exact path="/" component={Settings} />
+                            {/* <Route exact path="/" component={Settings} /> */}
                         </Switch>
                         </div>
                     </div>
